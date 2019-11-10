bff9646c9c8805ee350973026decf0f68eda3729
usstssl <ironhead199805@gmail.com>
2019-11-10 16:41:38 +0800

    Site updated: 2019-11-10 16:41:37

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,10 +1,9 @@
 /*页面载入完成后，创建复制按钮*/
 !function (e, t, a) { 
-  /* code */
   var initCopyCode = function(){
     var copyHtml = '';
     copyHtml += '<button class="btn-copy" data-clipboard-snippet="">';
-    copyHtml += '  <i class="fa fa-clipboard"></i><span>copy</span>';
+    copyHtml += '  <i class="fa fa-globe"></i><span>copy</span>';
     copyHtml += '</button>';
     $(".highlight .code pre").before(copyHtml);
     new ClipboardJS('.btn-copy', {
